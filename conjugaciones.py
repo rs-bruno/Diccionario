4ee8d9a05c56beb973ef61032b6b395c5e5b6098
Bruno <agusty48@gmail.com>
2022-02-15 11:46:04 -0300

    Programa principal básico.

--- conjugaciones.py
+++ conjugaciones.py
@@ -1,1 +1,11 @@
 import sys
+
+def generar(path):
+    '''Genera un archivo con las conjugaciones de todos los verbos encontrados en el archivo cuya ruta es *path*.'''
+    verbos = []
+    conjugaciones = []
+    print(f'Se conjugaron {len(verbos)} verbos.')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        generar(sys.argv[1])
