ef3c41cf2450c9d69a1a42798f289ef3db80dbc4
Bruno <agusty48@gmail.com>
2022-02-15 08:58:32 -0300

    Primer commit.

---
+++ porcentuador.py
@@ -0,0 +1,8 @@
+import sys
+
+def porcentuar(path):
+    '''Calcula una estimación del porcentaje del lenguaje utilizado.'''
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        porcentuar(sys.argv[1])
